2ceee58150076596e1a8cba2ee195118cb7b349b
nullsploit01 <harshaldharmik099@gmail.com>
2025-03-29 19:06:48 +0530

    add textures and font

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import "./App.css";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { TextureLoader } from "three";
 
 const App = () => {
-  return <div>App</div>;
+  const matcapTexture = useLoader(
+    TextureLoader,
+    "./src/assets/textures/matcaps/1.png"
+  );
+
+  return (
+    <div>
+      <Canvas className="webgl">
+        <mesh>
+          <ambientLight intensity={0.1} />
+          <directionalLight color="red" position={[0, 0, 5]} />
+          <boxGeometry args={[2, 2, 2]} />
+          <meshMatcapMaterial matcap={matcapTexture} />
+        </mesh>
+      </Canvas>
+    </div>
+  );
 };
 
 export default App;
