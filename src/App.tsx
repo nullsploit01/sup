bf1ef115dbaba652535cdaec136da56155863089
nullsploit01 <harshaldharmik099@gmail.com>
2025-03-29 19:35:22 +0530

    add ability to render 3d text

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,42 @@
+import { OrbitControls, PerspectiveCamera, Text3D } from "@react-three/drei";
 import "./App.css";
 import { Canvas, useLoader } from "@react-three/fiber";
 import { TextureLoader } from "three";
 
 const App = () => {
-  const matcapTexture = useLoader(
-    TextureLoader,
-    "./src/assets/textures/matcaps/1.png"
-  );
+  const matcapTexture = useLoader(TextureLoader, "/textures/matcaps/1.png");
 
   return (
     <div>
-      <Canvas className="webgl">
+      <Canvas
+        className="webgl"
+        style={{ width: "100vw", height: "100vh" }}
+        camera={{
+          fov: 75,
+          aspect: window.innerWidth / window.innerHeight,
+          near: 1,
+          far: 100,
+        }}
+      >
         <mesh>
           <ambientLight intensity={0.1} />
           <directionalLight color="red" position={[0, 0, 5]} />
-          <boxGeometry args={[2, 2, 2]} />
-          <meshMatcapMaterial matcap={matcapTexture} />
+          <Text3D
+            font={"/fonts/helvetiker_regular.typeface.json"}
+            size={1}
+            height={0.3}
+            curveSegments={12}
+            bevelEnabled
+            bevelThickness={0.03}
+            bevelSize={0.02}
+            bevelOffset={0}
+            bevelSegments={5}
+          >
+            Hello R3F!
+            <meshMatcapMaterial matcap={matcapTexture} />
+          </Text3D>
         </mesh>
+        <OrbitControls makeDefault />
       </Canvas>
     </div>
   );
